ea9a72daa165e358cdb0d8045d882d51895b6562
Sergey Isupov <isupov.s@outlook.com>
2021-12-15 09:51:54 +0300

    Added demo for ShapeLib

---
+++ SharpLibDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using ShapeLib;
+
+const int count = 100;
+var random = new Random();
+
+var shapes = new List<IShape>(count);
+for (int i = 0; i < count; i++)
+{
+    shapes.Add(GenerateShape(random.Next(2) > 0));
+}
+
+foreach (var shape in shapes)
+{
+    Console.WriteLine(shape.Area);
+}
+
+IShape GenerateShape(bool triangle)
+{
+    return triangle ? new Circle(10) : new Triangle(1, 3, 3);
+}
+
+
